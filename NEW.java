7f38ebaa18697252424edf2a91ad22cb63ba5dbf
umed <divyamlather509740@gmail.com>
2022-09-13 11:36:50 -0700

    first commit

---
+++ NEW.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class NEW {
+    public static void main(String[] args){
+        int[] myInts = new int[5];
+
+        myInts[0] = 00;
+        myInts[1] = 11;
+        myInts[2] =  22;
+        myInts[3] = 33;
+        myInts[4]=44;
+
+        for (int i=0; i<5; i++){
+            System.out.println("\n myints[" + i + "] = " + myInts[i]);
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+}
